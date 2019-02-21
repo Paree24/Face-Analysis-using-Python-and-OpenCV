b7bf45bfa82531fbf407e4397de1912e41bed8e5
Pareekshith Katti <37375677+Paree24@users.noreply.github.com>
2019-02-21 23:39:41 +0530

    Add files via upload

--- FaceDetection GUI.py
+++ FaceDetection GUI.py
@@ -4,6 +4,7 @@ import cv2
 import tkinter as tk
 #FileDialog class contains methods related to File dialog
 from tkinter import filedialog
+
 #Face Recognition function is uses OpenCV framework
 def facedetection(name) :
 	#Prints version of OpenCV in console
@@ -48,9 +49,58 @@ def facedetection(name) :
         cv2.rectangle(myimage,(x,y),(x+w,y+h),(0,0,255),4)
     #it takes image,coordinate1,coordinate2,RGB,Thickness as arguments
     #display og image with rectangles
+    
+    #Slightly Profiled Face
+    #CascadeClassifier is used to classify using Haarcascade facedetection data i.e, face or not face
+    face_learner=cv2.CascadeClassifier(r"C:\Users\PareeKatti\Anaconda3\Library\etc\haarcascades\haarcascade_profileface.xml")
+    #detect multiscale is used to detect face, 10% of image is read at a time
+    myface=face_learner.detectMultiScale(myimagegrey,1.1,5)
+    #Myface matrix
+    print(myface)
+    #Number of faces
+    print("number of faces")
+    print(len(myface))
+    #For loop is used to draw rectangle around the faces
+    for(x,y,w,h) in myface :
+        cv2.rectangle(myimage,(x,y),(x+w,y+h),(0,0,255),4)
+    
+    #Eyes detection
+    eye_learner=cv2.CascadeClassifier(r"C:\Users\PareeKatti\Anaconda3\Library\etc\haarcascades\haarcascade_eye.xml")
+    #detect multiscale is used to detect eye, 10% of image is read at a time
+    myeye=eye_learner.detectMultiScale(myimagegrey,1.6,10,minSize=(30,30))
+    #Myeye matrix
+    print(myeye)
+    print(len(myeye))
+    #For loop is used to draw rectangle around the eyes
+    for(x,y,w,h) in myeye :
+        cv2.rectangle(myimage,(x,y),(x+w,y+h),(0,255,0),2)
+        
+    #mouth detection
+    mouth_learner=cv2.CascadeClassifier(r"C:\Users\PareeKatti\Anaconda3\Library\etc\haarcascades\haarcascade_mcs_mouth.xml")
+    #detect multiscale is used to detect eye, 10% of image is read at a time
+    mouth=mouth_learner.detectMultiScale(myimagegrey,1.9,9,minSize=(30,30))
+    #Mouth matrix
+    print(mouth)
+    print(len(mouth))
+    #For loop is used to draw rectangle around the mouth
+    for(x,y,w,h) in mouth :
+        cv2.rectangle(myimage,(x,y),(x+w,y+h),(255,0,0),2)     
+        
+    #nose detection
+    nose_learner=cv2.CascadeClassifier(r"C:\Users\PareeKatti\Anaconda3\Library\etc\haarcascades\haarcascade_mcs_nose.xml")
+    #detect multiscale is used to detect eye, 10% of image is read at a time
+    nose=nose_learner.detectMultiScale(myimagegrey,1.9,5)
+    #Nose matrix
+    print(nose)
+    print(len(nose))
+    #For loop is used to draw rectangle around the nose
+    for(x,y,w,h) in nose :
+        cv2.rectangle(myimage,(x,y),(x+w,y+h),(255,255,0),2) 
+    #Show Image
     cv2.imshow("MyImage",myimage)
     cv2.waitKey(0)
     cv2.destroyAllWindows()
+#Main Flow
 #Name stores the path of image
 name=""
 #Function to open file using filedialog class
@@ -67,10 +117,17 @@ Message='This a small Face Detection Script using Python and OpenCV library duri
 Message2='The code was revised and a GUI was added using Tkinter Framework for Python. '
 Msg=Message+Message2
 #Heading
-tk.Label(a,text="Face Recognition using OpenCV",font="Verdana 20 bold").pack(pady=5,fill="x")
+tk.Label(a,text="Face Recognition using OpenCV",font="Verdana 20 bold",bg="#98a453").pack(pady=5,fill="x")
 #Message
-tk.Message(a,text=Msg).pack()
+tk.Message(a,text=Msg,bg="#aaaaaa").pack()
+           
+#The Following Set of code is used to show the colors used for each facial feature           
+tk.Label(a,text="Legend:",font="Verdana 10 bold",bg="#98a453").pack(pady=5,fill="x")
+tk.Message(a,text="Face",bg="Red",fg="white").pack(fill='x')
+tk.Message(a,text="Eyes",bg="Green",fg="white").pack(fill='x')
+tk.Message(a,text="Mouth",bg="Blue",fg="white").pack(fill='x')
+tk.Message(a,text="Nose",bg="Cyan").pack(fill='x')
 #Button. On Click, it'll open a file dialog to open image
-tk.Button(a,text="Browse for the image",font="Verdana 10 bold",command=openfile).pack(fill="x",pady=5)
+tk.Button(a,text="Browse for the image",font="Verdana 10 bold",bg="#98a453",command=openfile).pack(fill="x",pady=5)
 #GUI should be looped till terminated
 a.mainloop()
